1a98640369a742fba874381e4248c97b1cff4c1e
VOVOplay <vovoplaygame3@gmail.com>
2026-09-17 21:48:38 +0200

    Theory of categories for sidebar

--- src/myTypes/article.go
+++ src/myTypes/article.go
@@ -13,7 +13,44 @@ type ArticleInfo struct {
 	Author string
 }
 
+type Category struct {
+	Name     string
+	Elements []CategoryItem
+}
+
+type CategoryItem interface {
+	isCategoryItem()
+}
+
+func (a *Article) isCategoryItem()  {}
+func (c *Category) isCategoryItem() {}
+
 // Articles:
 // []Article
 // Input: /something/perks
 // Resolves path on the fly? Or struct for
+
+/*
+
+Category {
+	Article
+	Article
+	Article
+	Category {
+		Article
+		Article
+	}
+	Article
+	Category {
+		Article
+		Article
+		Article
+	}
+}
+
+Category {
+	Article
+	Article
+}
+
+*/
